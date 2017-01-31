1d7460d9c3ab839f96fd14a62afe4be115d1e466
sandrobirke <sandro.birke@gmail.com>
2017-01-31 16:11:08 +0100

    Add Dokumenteneliste, Custom Styles and LESS

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,35 @@
 import { Component } from '@angular/core';
 
-
-export class Hero{
+export class Dokumente {
   id: number;
   name: string;
 }
+
+const DOKUMENTE: Dokumente[] = [
+  { id: 11, name: 'Stadtratssitzung von 19.01.2016'},
+  { id: 12, name: 'Bauantrag Grubenstraße 12'},
+  { id: 13, name: 'Gemeinderatsbeschluss Nr 94-112'},
+  { id: 14, name: 'Gemeinderatsbeschluss Nr 87-010'},
+  { id: 15, name: 'Vorlage Gemeinderatssizung vom20.12.2014'},
+  { id: 16, name: 'Mitgliederliste Gemeinderat Torgau'}
+]
 
 @Component({
   selector: 'my-app',
   template: `
             <h1>{{title}}</h1>
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
-            <div>
-              <label>name: </label>
-              <input [(ngModel)]="hero.name" placeholder="name" />
-            </div>
+            <h2>Meine Dokumente</h2>
+            <ul class="documents">
+              <li *ngFor="let dokument of documente">
+                <span class="badge">{{dokument.id}}</span> {{dokument.name}}
+              </li>
+            </ul>
             `,
+  styleUrls: ['./app/app.component.css']
 })
 export class AppComponent  {
 
-  title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  title = 'Aktentasche';
+  documente = DOKUMENTE;
 
 }
