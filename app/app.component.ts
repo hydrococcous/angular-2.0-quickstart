8948c3be9528d62af9e845adb5f144c96e1962fa
sandrobirke <sandro.birke@gmail.com>
2017-02-01 17:20:37 +0100

    add 3rd routing component, add styles for active links

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <h1>Hello {{name}}</h1>
-    <hr />
-    <nav>
-      <a routerLink="/one">Route 1</a>
-      <a routerLink="/two">Route 2</a>
-      <a routerLink="/three">Route 3</a>
-    </nav>
-    <router-outlet></router-outlet>
-    `,
+	selector: 'my-app',
+	template: `
+		<h1>Hello {{name}}</h1>
+		<hr />
+		<nav>
+			<a routerLink="/one" routerLinkActive="active">Route 1</a>
+			<a routerLink="/two" routerLinkActive="active">Route 2</a>
+			<a routerLink="/three" routerLinkActive="active">Route 3</a>
+		</nav>
+		<router-outlet></router-outlet>
+		`,
+	styles: [`
+		A.active{
+			color: #ccc;
+			cursor: default;
+			text-decoration: none;
+		}
+	`]
 })
 export class AppComponent  {
-  name = 'Angular 2.0 Routing Module';
+    name = 'Angular 2.0 Routing Module';
 }
